aedb506435f033b7b95089fe16977799fb5ad100
mompei <ogunborjerry@gmail.com>
2023-08-31 11:34:53 +0100

    Optimized images, add critical score to gamecard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,8 @@ import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import { Game } from '../hooks/useGames'
 import PlatformIconList from './PlatformIconList'
+import CriticScore from './CriticScore'
+import getCroppedImageUrl from '../services/image-url'
 
 interface Props {
     game: Game
@@ -10,10 +12,13 @@ interface Props {
 const GameCard = ({ game }: Props) => {
     return (
     <Card borderRadius={10} overflow= 'hidden'>
-    <Image src={game.background_image} />
+     <Image src={getCroppedImageUrl(game.background_image)} />
     <CardBody>
     <Heading fontSize='2xl'>{game.name}</Heading>
+    <HStack justifyContent='space-between' marginBottom={3}>
     <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
+    <CriticScore score={game.metacritic} />
+    </HStack>
     </CardBody>
     </Card>
     )
